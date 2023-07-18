cd031796db17dbfc25245cb0482e3e3be440e139
Lari <larileppanen@hotmail.com>
2023-07-18 18:41:24 +0300

    phonebook step6 add db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
+import axios from 'axios';
 
 
 const Persons = ({ persons, newFilter }) => {
@@ -55,12 +56,7 @@ const PersonForm = ({
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setName] = useState('')
   const [newNumber, setNumber] = useState('')
   const [newFilter, setFilter] = useState('')
@@ -75,6 +71,24 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+
+  const hook = () => {
+  //  console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+    //    console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+ // console.log('render', persons.length, 'persons')
+
+
+
+
+ 
 const addPerson =(event) => {
     event.preventDefault()
 
