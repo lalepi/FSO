e02c552449d51dbf42f4a11df1cb7cf3070cfc19
Lari <larileppanen@hotmail.com>
2023-11-11 13:23:26 +0200

    Add userExtractor
    Added extraction of user id, currently it is used directly in blog post
    & delete, since it makes easier to develop. Otherwise it would need an
    token with get command also.

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (request, response, next) => {
     logger.info('Method:', request.method)
@@ -30,12 +32,20 @@ const errorHandler = (error, request, response, next) => {
     next(error)
 }
 
-const tokenExtractor = (request, response, next) => {
-    const authorization = request.get('authorization')
+const tokenExtractor = async (request, response, next) => {
+    const authorization = await request.get('authorization')
     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
         request.token = authorization.substring(7)
         next()
     } else {
+        next()
+    }
+}
+const userExtractor = async (request, response, next) => {
+
+    if (request.token) {
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        request.user = await User.findById(decodedToken.id)
         next()
     }
 }
@@ -44,5 +54,6 @@ module.exports = {
     requestLogger,
     unknownEndpoint,
     errorHandler,
-    tokenExtractor
+    tokenExtractor,
+    userExtractor
 }
