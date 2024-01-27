913e948c817b12bb94e0cff97a2b288c4d30b9e9
DESKTOP-UCI7A6N\Dell <larileppanen@hotmail.com>
2024-01-27 17:31:42 +0200

    Part 6.20 add Anecdote React Query

---
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,39 @@
+import { useQuery, useMutation, useQueryClient} from 'react-query'
+import { getAnecdotes, createAnecdote } from '../requests'
+
+
+
+
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+
+      console.log("AnecdoteForm anecdotes", anecdotes)
+    },
+  })
+  
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+    console.log('new anecdote')
+}
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name='anecdote' />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
