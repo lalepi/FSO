e8c6ece7206e77375c53b71d21738b3af05b0d59
DESKTOP-UCI7A6N\Dell <larileppanen@hotmail.com>
2024-05-07 18:26:47 +0300

    fix part 9.7

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -1,83 +1,65 @@
-const exerciseCalculator = (exercises: object, target: number) => {
-  interface exercise {
-    training: {
-      periodLength: number;
-      trainingDays: number;
-      success: boolean;
-      averageTime: number;
-      rating: number;
-      ratingDescription: string;
-      target: number;
-    };
-  }
+interface exercise {
+  training: {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    averageTime: number;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+  };
+}
 
-  const parseArguments = (exercises: object, target: number): exercise => {
-    const periodLength = Object.keys(exercises).length;
+const exerciseCalculator = (exercises: object, target: number): exercise => {
+  const periodLength = Object.keys(exercises).length;
 
-    const trainingHours = Object.values(exercises).flatMap((hours) => {
-      if (hours <= 0) return [];
-      if (isNaN(Number(hours)))
-        throw new Error("Provided values were not numbers!");
-      else return Number(hours);
-    });
+  const trainingHours = Object.values(exercises).flatMap((hours) => {
+    if (hours <= 0) return [];
+    else return Number(hours);
+  });
 
-    const trainingDays = trainingHours.length;
+  const trainingDays = trainingHours.length;
 
-    const total = trainingHours.reduce((acc, hour) => acc + hour);
-    const totalAVG = total / trainingHours.length;
+  const total = trainingHours.reduce((acc, hour) => acc + hour);
+  const totalAVG = total / trainingHours.length;
 
-    const targetValue = Number(target);
+  const targetValue = Number(target);
 
-    const rating = () => {
-      switch (true) {
-        case totalAVG > targetValue * 1.5:
-          return {
-            desc: "Fantastic",
-            success: true,
-            rating: 1,
-          };
+  const rating = () => {
+    switch (true) {
+      case totalAVG > targetValue * 1.5:
+        return {
+          desc: "Fantastic",
+          success: true,
+          rating: 1,
+        };
+      case totalAVG >= targetValue:
+        return {
+          desc: "You are doing great",
+          success: true,
+          rating: 2,
+        };
+      default:
+        return {
+          desc: "keep going",
+          success: false,
+          rating: 3,
+        };
+    }
+  };
+  const verdict = rating();
 
-        case totalAVG > targetValue:
-          return {
-            desc: "You are doing great",
-            success: true,
-            rating: 2,
-          };
-        case totalAVG < targetValue:
-          return {
-            desc: "keep going",
-            success: false,
-            rating: 3,
-          };
-        default:
-          throw new Error("parameters missing");
-      }
-    };
-    const verdict = rating();
-
-    const training = {
-      periodLength: periodLength,
-      trainingDays: trainingDays,
-      success: verdict.success,
-      rating: verdict.rating,
-      ratingDescription: verdict.desc,
-      target: targetValue,
-      averageTime: totalAVG,
-    };
-
-    return { training };
+  const training = {
+    periodLength: periodLength,
+    trainingDays: trainingDays,
+    success: verdict.success,
+    rating: verdict.rating,
+    ratingDescription: verdict.desc,
+    target: targetValue,
+    averageTime: totalAVG,
   };
 
-  try {
-    const { training } = parseArguments(exercises, target);
-    return training;
-  } catch (error: unknown) {
-    let errorMessage = "Something bad happened.";
-    if (error instanceof Error) {
-      errorMessage += " Error: " + error.message;
-    }
-    return errorMessage;
-  }
+  return { training };
 };
 
 export default exerciseCalculator;
--- part9/index.ts
+++ part9/index.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import express from "express";
 import BmiCalculator from "./bmiCalculator";
@@ -5,40 +8,32 @@ import exerciseCalculator from "./exerciseCalculator";
 
 const app = express();
 app.use(express.json());
-app.get("/hello", (_req, res) => {
-  res.send("Hello Full Stack!");
-});
 
 app.get("/bmi", (req, res) => {
   const { height, weight } = req.query;
-
   res.json(BmiCalculator(Number(height), Number(weight)));
 });
 
 app.post("/exercises", (req, res) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
-  const exercises = Object(req.body.daily_exercises);
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const target = Number(req.body.target);
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   if (!req.body.daily_exercises || !req.body.target) {
     return res.status(400).json({
       error: "parameters missing",
     });
   }
-  if (isNaN(req.body.target)) {
+  if (
+    !Array.isArray(req.body.daily_exercises) ||
+    req.body.daily_exercises.some(isNaN) ||
+    isNaN(req.body.target)
+  ) {
     return res.status(400).json({
-      error: "parameters missingol",
+      error: "malformatted parameters",
     });
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
-  const result = exerciseCalculator(exercises, target);
+  const exercises = Object(req.body.daily_exercises);
+  const target = Number(req.body.target);
 
-  console.log("result", result);
+  const result = exerciseCalculator(exercises, target);
 
   return res.send({ result });
 });
